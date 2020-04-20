136c91a01a1f3bb20d76695206b5d92226bd95d5
Justin Peel <justinpeel25@gmail.com>
2020-04-20 18:53:14 +0200

    Initial Commit

---
+++ FizzBizzWhiz/FizzBizzUseCase.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using NUnit;
+using NUnit.Framework;
+
+namespace FizzBizzWhiz
+{
+	[TestFixture]
+	public class FizzBizzWhiz
+	{
+		[TestCase(1, "1")]
+		[TestCase(2, "2")]
+		[TestCase(4, "4")]
+		public void GivenNaturalNumber_IsNotDivisibleBy3or5_shouldReturnNumber(int number, string expected)
+		{
+			//act
+			var actual = FizzBuzz(number);
+
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestCase(3, "Fizz")]
+		[TestCase(6, "Fizz")]
+		[TestCase(9, "Fizz")]
+		public void GivenNaturalNumber_IsDivisbleBy3_ShouldReturnFizz(int number, string expected)
+		{
+			//act
+			var actual = FizzBuzz(number);
+
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestCase(5, "Buzz")]//arrange
+		[TestCase(10, "Buzz")]
+		[TestCase(20, "Buzz")]
+		public void GivenNaturalNumber_IsDivisibleBy5_ShouldReturnBuzz(int number, string expected)
+		{
+			//act
+			var actual = FizzBuzz(number);
+
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestCase(15, "FizzBuzz")]//arrange
+		[TestCase(30, "FizzBuzz")]
+		[TestCase(75,"FizzBuzz")]
+		public void GivenNaturalNumber_IsDivisbleBy3And5_shouldReturnFizzBuzz(int number, string expected)
+		{
+			//act
+			var actual = FizzBuzz(number);
+
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		private string FizzBuzz(int number)
+		{
+			if (number % 5 == 0 && number % 3 == 0)
+				return "FizzBuzz";
+			else if (number % 3 == 0)
+				return "Fizz";
+			else if (number % 5 == 0)
+				return "Buzz";
+			else
+				return number.ToString();
+		
+		}
+	}
+}
---
+++ FizzBizzWhiz/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FizzBizzWhiz")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FizzBizzWhiz")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1e8bf9fd-68d8-4ca6-898f-1af1ea282496")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
